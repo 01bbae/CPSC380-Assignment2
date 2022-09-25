08c321abdaf7964e93f6dac6114d20e13adb01ab
BJ <01bbae@gmail.com>
2022-09-25 00:51:43 -0700

    added wait and fork functionality

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Assignment: Assignment 2 - Simple Shell
 References:
 
 1. https://stackoverflow.com/questions/9673708/tell-cin-to-stop-reading-at-newline
+2. https://man7.org/linux/man-pages/man2/wait.2.html#EXAMPLES
+3. 
--- sshell.cpp
+++ sshell.cpp
@@ -11,7 +11,8 @@ int main(int argc, char const *argv[])
     
     char *args[MAX_LINE/2 + 1]; /* max of 40 arguments */
     int should_run = 1;
-    int i, upper;
+    int i, upper, wstatus;
+    pid_t pid, w;
 
     while (should_run) {   
         printf("osh>");
@@ -20,15 +21,32 @@ int main(int argc, char const *argv[])
         /** fork a child to execute command (using one of the exec calls)
              Break out of loop if user types ‘quit’ or ‘exit’ then exit   
         program **/
-        string userInput = "";
-        cin >> userInput;
-        if (userInput == "quit" || userInput == "exit"){
+        cin.getline(*args, (streamsize) MAX_LINE, ' '); // second argument is of type streamsize and not int
+        if (args[0] == "quit" || args[0] == "exit"){
             should_run = 0;
-        }else if(!userInput.empty() and userInput != "\n"){
-            userInput.pop_back();
-            // pid_t fork();
-            // exec();
+        }else{
+            pid = fork(); //errors on my mingw env on windows (unistd not included?)
+            switch (pid)
+            {
+            case -1:
+                perror("fork creation failed");
+                break;
+            case 0:
+                execl(args)
+                printf("fork finished");
+                break;
+            default:
+                printf("parent finished");
+
+                // waiting for children to finish
+                w = wait(&wstatus);
+                if(!WIFEXITED(wstatus)){
+                    printf("child not terminated normally");
+                }
+                break;
+            }
         }
+        *args = NULL;
    }
 
     return 0;
