9ce4b5582cda2b8ce26e1bbd8f326f93977dc3b7
BJ Bae <01bbae@gmail.com>
2022-09-25 05:17:05 -0700

    Middle of refactoring to using string

--- sshell.cpp
+++ sshell.cpp
@@ -7,47 +7,54 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    printf("Works!");
-    
-    char *args[MAX_LINE/2 + 1]; /* max of 40 arguments */
+    printf("Works!\n");
+
+    // char *args[MAX_LINE / 2 + 1]; /* max of 40 arguments */
+    string args;
     int should_run = 1;
     int i, upper, wstatus;
     pid_t pid, w;
 
-    while (should_run) {   
+    while (should_run)
+    {
         printf("osh>");
         fflush(stdout);
-        
+
         /** fork a child to execute command (using one of the exec calls)
-             Break out of loop if user types ‘quit’ or ‘exit’ then exit   
+             Break out of loop if user types ‘quit’ or ‘exit’ then exit
         program **/
-        cin.getline(*args, (streamsize) MAX_LINE, ' '); // second argument is of type streamsize and not int
-        if (args[0] == "quit" || args[0] == "exit"){
+        getline(cin, args); // second argument is of type streamsize and not int
+        cout << args << endl;
+
+        if (args == "exit" || args == "quit")
+        {
             should_run = 0;
-        }else{
-            pid = fork(); //errors on my mingw env on windows (unistd not included?)
+        }
+        else
+        {
+            pid = fork(); // errors on my mingw env on windows (unistd not included?)
             switch (pid)
             {
             case -1:
                 perror("fork creation failed");
                 break;
             case 0:
-                execl(args)
-                printf("fork finished");
+                // execlp(args);
+                printf("fork finished\n");
                 break;
             default:
-                printf("parent finished");
+                printf("parent finished\n");
 
                 // waiting for children to finish
                 w = wait(&wstatus);
-                if(!WIFEXITED(wstatus)){
-                    printf("child not terminated normally");
+                if (!WIFEXITED(wstatus))
+                {
+                    printf("child not terminated normally\n");
                 }
                 break;
             }
         }
-        *args = NULL;
-   }
+    }
 
     return 0;
 }
